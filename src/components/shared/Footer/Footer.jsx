b7378e60ac68479794c0e94c8420a70857e340c8
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-04-14 18:51:25 +0600

    added footer but not responsive

---
+++ src/components/shared/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import footerImg from "../../../assets/images/logo.png";
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
+import { RiTwitterXFill } from "react-icons/ri";
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#001931] mt-10 py-10 ">
+      <div className="container  text-white mx-auto">
+        <div className="flex items-center justify-between gap-5">
+          <div className="flex items-center gap-2 ">
+            <img className="w-7" src={footerImg} alt="footerImg" />
+            <h1 className="font-bold">HERO .IO</h1>
+          </div>
+          <div className="">
+            <h1 className="font-semibold">Social Links</h1>
+            <div className="flex items-center gap-3 mt-3">
+              <a href="#"><FaFacebook /></a>
+              <a href="#"><FaLinkedin /></a>
+              <a href="#"><RiTwitterXFill /></a>
+            </div>
+          </div>
+        </div>
+        <hr className="text-gray-400 my-3" />
+        <p className="text-gray-500 text-center">Copyright @ 2025 - All right reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Nevber from "../components/shared/Navber/Navber";
+import Footer from "../components/shared/Footer/Footer";
 
 const RootLayout = () => {
   return (
     <div>
       <Nevber></Nevber>
       <Outlet></Outlet>
-      <h1>footer</h1>
+      <Footer></Footer>
     </div>
   );
 };
